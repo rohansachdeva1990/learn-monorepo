21c1f45e6caef9fc6559618dd939eb1a8a5afb4b
Rohan Sachdeva <rohan.sachdeva1990@gmail.com>
2021-09-20 21:52:13 +0200

    monorepo with react apps using yarn workspace

---
+++ using-yarn-workspaces/react-based/team-a/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+import { Button } from 'core';
+
+export function App() {
+  return (
+    <div className='App'>
+      <h1>Hello from Team A</h1>
+      <Button />
+    </div>
+  );
+}
+
+// export default App;
